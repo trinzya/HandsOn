2065453eb72d7f86e379d63eeb4ce49465f51563
trinzya <59994401+trinzya@users.noreply.github.com>
2021-06-22 10:32:50 +0530

    Comment Added

--- FlightManagemnt/src/main/java/com/cognizant/flightmanagement/repository/IPassengerRepository.java
+++ FlightManagemnt/src/main/java/com/cognizant/flightmanagement/repository/IPassengerRepository.java
@@ -1,5 +1,5 @@
 package com.cognizant.flightmanagement.repository;
-
+//import nescessary packages
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.cognizant.flightmanagement.model.Passenger;
 @Repository
 public interface IPassengerRepository extends JpaRepository<Passenger, String> {
+	//This function is used to filter the passenger based on column  Vip
 	public List<Passenger>   findByVip(boolean vip);
 	
 
